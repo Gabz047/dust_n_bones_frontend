d69e71f47ca3cd32685280b61275204f3282cf13
Gabz047 <gabe1612mail@gmail.com>
2025-10-24 11:02:32 -0300

    FEAT: Added pwa

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,40 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate', // atualiza SW automaticamente
+      manifest: {
+        name: 'Dust N Bones',
+        short_name: 'DustNBones',
+        description: 'App de listagem de ossos para lab veterinário',
+        theme_color: '#4caf50',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/dustnbones.jpeg',
+            sizes: '192x192',
+            type: 'image/jpeg'
+          },
+          {
+            src: '/dustnbones.jpeg',
+            sizes: '512x512',
+            type: 'image/jpeg'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,jpeg,png,svg,ico}']
+      }
+    })
   ],
   resolve: {
     alias: {
